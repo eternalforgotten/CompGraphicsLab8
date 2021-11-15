22e1ae82bb6d8267498668881795b0613c7de4aa
dkicenko <61902129+dkicenko@users.noreply.github.com>
2021-11-16 00:27:48 +0300

    2 task

---
+++ Lab8/Buffer.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Lab8
+{
+    class Buffer
+    {
+        private static int ProjMode = 0;
+        public static Bitmap CreateZBuffer(int width, int height, List<Figure> scene, List<Color> colors, int projMode = 0)
+        {
+            ProjMode = projMode;
+
+            Bitmap newImg = new Bitmap(width, height);
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    newImg.SetPixel(i, j, Color.White);
+
+            float[,] zbuff = new float[width, height];
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    zbuff[i, j] = float.MinValue;
+
+            List<List<List<Point3D>>> rasterizedScene = new List<List<List<Point3D>>>();
+            for (int i = 0; i < scene.Count; i++)
+            {
+                rasterizedScene.Add(MakeRasterization(scene[i]));
+            }
+
+            var centerX = width / 2;
+            var centerY = height / 2;
+
+            int ind = 0;
+            for (int i = 0; i < rasterizedScene.Count; i++)
+            {
+                //Смещение по центру фигуры
+                var figureLeftX = rasterizedScene[i].Where(face => face.Count != 0).Min(face => face.Min(vertex => vertex.X));
+                var figureLeftY = rasterizedScene[i].Where(face => face.Count != 0).Min(face => face.Min(vertex => vertex.Y));
+                var figureRightX = rasterizedScene[i].Where(face => face.Count != 0).Max(face => face.Max(vertex => vertex.X));
+                var figureRightY = rasterizedScene[i].Where(face => face.Count != 0).Max(face => face.Max(vertex => vertex.Y));
+                var figureCenterX = (figureRightX - figureLeftX) / 2;
+                var figureCenterY = (figureRightY - figureLeftY) / 2;
+
+                Random r = new Random();
+
+                for (int j = 0; j < rasterizedScene[i].Count; j++)
+                {
+                    List<Point3D> curr = rasterizedScene[i][j];
+                    foreach (Point3D point in curr)
+                    {
+                        int x = (int)(point.X + centerX - figureCenterX);
+                        int y = (int)(point.Y + centerY - figureCenterY);
+                        if (x < width && y < height && x > 0 && y > 0)
+                        {
+                            if (point.Z > zbuff[x, y])
+                            {
+                                zbuff[x, y] = point.Z;
+                                newImg.SetPixel(x, y, colors[ind % colors.Count]);
+                            }
+                        }
+                    }
+                    ind++;
+                }
+            }
+            return newImg;
+        }
+
+        private static List<List<Point3D>> MakeRasterization(Figure fig)
+        {
+            List<List<Point3D>> list_rast = new List<List<Point3D>>();
+            foreach (var surf in fig.Surfaces)
+            {
+                List<Point3D> cursurface = new List<Point3D>();
+                List<Point3D> surfacePoints = new List<Point3D>();
+                for (int i = 0; i < surf.Count; i++)
+                {
+                    surfacePoints.Add(fig.Vertexes[surf[i]]);
+                }
+
+                List<List<Point3D>> triangles = Triangulate(surfacePoints);
+                foreach (List<Point3D> triangle in triangles)
+                {
+                    cursurface.AddRange(RasterizeTriangle(MakeProj(triangle)));
+                }
+                list_rast.Add(cursurface);
+            }
+            return list_rast;
+        }
+
+
+        private static List<Point3D> RasterizeTriangle(List<Point3D> points)
+        {
+            List<Point3D> res = new List<Point3D>();
+
+            points.Sort((point1, point2) => point1.Y.CompareTo(point2.Y));
+            var rpoints = points.Select(point => (X: (int)Math.Round(point.X), Y: (int)Math.Round(point.Y), Z: (int)Math.Round(point.Z))).ToList();
+
+            var x01 = Interpolate(rpoints[0].Y, rpoints[0].X, rpoints[1].Y, rpoints[1].X);
+            var x12 = Interpolate(rpoints[1].Y, rpoints[1].X, rpoints[2].Y, rpoints[2].X);
+            var x02 = Interpolate(rpoints[0].Y, rpoints[0].X, rpoints[2].Y, rpoints[2].X);
+
+            var z01 = Interpolate(rpoints[0].Y, rpoints[0].Z, rpoints[1].Y, rpoints[1].Z);
+            var z12 = Interpolate(rpoints[1].Y, rpoints[1].Z, rpoints[2].Y, rpoints[2].Z);
+            var z02 = Interpolate(rpoints[0].Y, rpoints[0].Z, rpoints[2].Y, rpoints[2].Z);
+
+            x01.RemoveAt(x01.Count - 1);
+            List<int> x012 = x01.Concat(x12).ToList();
+
+            z01.RemoveAt(z01.Count - 1);
+            List<int> z012 = z01.Concat(z12).ToList();
+
+            int middle = x012.Count / 2;
+            List<int> leftX, rightX, leftZ, rightZ;
+            if (x02[middle] < x012[middle])
+            {
+                leftX = x02;
+                leftZ = z02;
+                rightX = x012;
+                rightZ = z012;
+            }
+            else
+            {
+                leftX = x012;
+                leftZ = z012;
+                rightX = x02;
+                rightZ = z02;
+            }
+
+            int y0 = rpoints[0].Y;
+            int y2 = rpoints[2].Y;
+
+            for (int ind = 0; ind <= y2 - y0; ind++)
+            {
+                int XL = leftX[ind];
+                int XR = rightX[ind];
+
+                List<int> intCurrZ = Interpolate(XL, leftZ[ind], XR, rightZ[ind]);
+
+                for (int x = XL; x < XR; x++)
+                {
+                    res.Add(new Point3D(x, y0 + ind, intCurrZ[x - XL]));
+                }
+            }
+
+            return res;
+        }
+
+
+        private static List<List<Point3D>> Triangulate(List<Point3D> points)
+        {
+            if (points.Count == 3)
+                return new List<List<Point3D>> { points };
+
+            List<List<Point3D>> res = new List<List<Point3D>>();
+            for (int i = 2; i < points.Count; i++)
+            {
+                res.Add(new List<Point3D> { points[0], points[i - 1], points[i] });
+            }
+
+            return res;
+        }
+
+        
+        private static List<int> Interpolate(int y0, int x0, int y1, int x1)
+        {
+            if (y0 == y1)
+            {
+                return new List<int> { x0 };
+            }
+            List<int> res = new List<int>();
+
+            float step = (x1 - x0) * 1.0f / (y1 - y0);
+            float value = x0;
+            for (int i = y0; i <= y1; i++)
+            {
+                res.Add((int)value);
+                value += step;
+            }
+
+            return res;
+        }
+
+        public static List<Point3D> MakeProj(List<Point3D> init) => new Projection().ProjectZBuff(init, ProjMode);
+    }
+}
--- Lab8/Form1.cs
+++ Lab8/Form1.cs
@@ -7,14 +7,454 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Math;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Lab8
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        Pen pen;
+        Projection projection;
+        Figure curFigure;
+        private List<Point3D> rotationPoints;
+        private List<Figure> scenes = new List<Figure>();
+        private List<Color> colors;
+
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+            pen = new Pen(Color.BlueViolet, 2);
+            projection = new Projection();
+            rotationPoints = new List<Point3D>();
+            projBox.SelectedIndex = 0;
+            radioButton1.Checked = true;
+
+
+            colors = new List<Color>();
+            Random r = new Random(Environment.TickCount);
+            for (int i = 0; i < 100; ++i)
+                colors.Add(Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255)));
+            
+        }
+        private void Draw()
+        {
+
+            if (curFigure != null)
+            {
+                DeletePic();
+                g.Clear(Color.White);
+                List<Edge> edges = projection.Project(curFigure, projBox.SelectedIndex);
+
+                var centerX = pictureBox1.Width / 2;
+                var centerY = pictureBox1.Height / 2;
+
+                var figureLeftX = edges.Min(e => e.From.X < e.To.X ? e.From.X : e.To.X);
+                var figureLeftY = edges.Min(e => e.From.Y < e.To.Y ? e.From.Y : e.To.Y);
+                var figureRightX = edges.Max(e => e.From.X > e.To.X ? e.From.X : e.To.X);
+                var figureRightY = edges.Max(e => e.From.Y > e.To.Y ? e.From.Y : e.To.Y);
+
+
+                var figureCenterX = (figureRightX - figureLeftX) / 2;
+                var figureCenterY = (figureRightY - figureLeftY) / 2;
+
+
+                foreach (Edge line in edges)
+                {
+                    var p1 = line.From.To2DPoint();
+                    var p2 = line.To.To2DPoint();
+                    g.DrawLine(pen, p1.X + centerX - figureCenterX, p1.Y + centerY - figureCenterY, p2.X + centerX - figureCenterX, p2.Y + centerY - figureCenterY);
+                }
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point3D start = new Point3D(0, 0, 0);
+            float len = 150;
+
+            List<Point3D> points = new List<Point3D>
+            {
+                start,
+                new Point3D(len, 0, 0),
+                new Point3D(len, 0, len),
+                new Point3D(0, 0, len),
+
+                new Point3D(0, len, 0),
+                new Point3D(len, len, 0),
+                new Point3D(len, len, len),
+                new Point3D(0, len, len)
+            };
+
+            curFigure = new Figure(points);
+            curFigure.AddEdges(0, new List<int> { 1, 4 });
+            curFigure.AddEdges(1, new List<int> { 2, 5 });
+            curFigure.AddEdges(2, new List<int> { 6, 3 });
+            curFigure.AddEdges(3, new List<int> { 7, 0 });
+            curFigure.AddEdges(4, new List<int> { 5 });
+            curFigure.AddEdges(5, new List<int> { 6 });
+            curFigure.AddEdges(6, new List<int> { 7 });
+            curFigure.AddEdges(7, new List<int> { 4 });
+
+            curFigure.AddSurface(new List<int> { 0, 1, 2, 3 });
+            curFigure.AddSurface(new List<int> { 1, 2, 6, 5 });
+            curFigure.AddSurface(new List<int> { 0, 3, 7, 4 });
+            curFigure.AddSurface(new List<int> { 4, 5, 6, 7 });
+            curFigure.AddSurface(new List<int> { 2, 3, 7, 6 });
+            curFigure.AddSurface(new List<int> { 0, 1, 5, 4 });
+
+            scenes.Add(curFigure);
+            Draw();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Point3D start = new Point3D(0, 0, 0);
+            float len = 150;
+
+            List<Point3D> points = new List<Point3D>
+            {
+                start,
+                new Point3D(len, 0, len),
+                new Point3D(len, len, 0),
+                new Point3D(0, len, len),
+            };
+
+            curFigure = new Figure(points);
+            curFigure.AddEdges(0, new List<int> { 1, 3, 2 });
+            curFigure.AddEdges(1, new List<int> { 3 });
+            curFigure.AddEdges(2, new List<int> { 1, 3 });
+
+
+            curFigure.AddSurface(new List<int> { 0, 1, 2 });
+            curFigure.AddSurface(new List<int> { 0, 1, 3 });
+            curFigure.AddSurface(new List<int> { 0, 2, 3 });
+            curFigure.AddSurface(new List<int> { 1, 2, 3 });
+
+            scenes.Add(curFigure);
+            Draw();
+        }
+       
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Point3D start = new Point3D(0, 0, 0);
+            float len = 150;
+
+            List<Point3D> points = new List<Point3D>
+            {
+                start,
+                new Point3D(len , len , 0),
+                new Point3D(-len, len , 0),
+                new Point3D(0, len , -len ),
+                new Point3D(0, len , len ),
+                new Point3D(0,  2 *len, 0),
+            };
+
+            curFigure = new Figure(points);
+            curFigure.AddEdges(0, new List<int> { 1, 3, 2, 4 });
+            curFigure.AddEdges(5, new List<int> { 1, 3, 2, 4 });
+            curFigure.AddEdges(1, new List<int> { 3 });
+            curFigure.AddEdges(3, new List<int> { 2 });
+            curFigure.AddEdges(2, new List<int> { 4 });
+            curFigure.AddEdges(4, new List<int> { 1 });
+
+            curFigure.AddSurface(new List<int> { 0, 1, 3 });
+            curFigure.AddSurface(new List<int> { 0, 1, 4 });
+            curFigure.AddSurface(new List<int> { 0, 2, 3 });
+            curFigure.AddSurface(new List<int> { 0, 2, 4 });
+            curFigure.AddSurface(new List<int> { 5, 1, 3 });
+            curFigure.AddSurface(new List<int> { 5, 1, 4 });
+            curFigure.AddSurface(new List<int> { 5, 2, 3 });
+            curFigure.AddSurface(new List<int> { 5, 2, 4 });
+
+            scenes.Add(curFigure);
+            Draw();
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            scenes.Clear();
+            pictureBox1.Invalidate();
+            rotationPoints.Clear();
+        }
+
+        private void DeletePic()
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            float x = float.Parse(textBox1.Text);
+            float y = float.Parse(textBox2.Text);
+            float z = float.Parse(textBox3.Text);
+            AffineChanges.Translate(curFigure, x, y, z);
+            Draw();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            float x = float.Parse(textBox1.Text) / 100;
+            float y = float.Parse(textBox2.Text) / 100;
+            float z = float.Parse(textBox3.Text) / 100;
+            AffineChanges.Scale(curFigure, x, y, z);
+            Draw();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            float x = float.Parse(textBox1.Text);
+            float y = float.Parse(textBox2.Text);
+            float z = float.Parse(textBox3.Text);
+            AffineChanges.Rotate(curFigure, x, y, z);
+            Draw();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            AffineChanges.Reflect(curFigure, "xy");
+            Draw();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            AffineChanges.Reflect(curFigure, "yz");
+            Draw();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            AffineChanges.Reflect(curFigure, "xz");
+            Draw();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            float a = float.Parse(textBox4.Text) / 100;
+            AffineChanges.ScaleCenter(curFigure, a);
+            Draw();
+        }
+        private void rotateBtn_Click(object sender, EventArgs e)
+        {
+            if (rotateOX.Checked)
+            {
+                AffineChanges.RotateCentral(curFigure, (float)rotateAngle.Value, 0, 0);
+            }
+            else if (rotateOY.Checked)
+            {
+                AffineChanges.RotateCentral(curFigure, 0, (float)rotateAngle.Value, 0);
+            }
+            else if (rotateOZ.Checked)
+            {
+                AffineChanges.RotateCentral(curFigure, 0, 0, (float)rotateAngle.Value);
+            }
+            else if (rotateOwn.Checked)
+
+            {
+                Edge ed = new Edge(float.Parse(rX1.Text), float.Parse(rY1.Text), float.Parse(rZ1.Text),
+                    float.Parse(rX2.Text), float.Parse(rY2.Text), float.Parse(rZ2.Text));
+                AffineChanges.RotateFigureAboutLine(curFigure, (float)rotateAngle.Value, ed);
+            }
+            Draw();
+        }
+
+        private void projBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (curFigure != null)
+                Draw();
+        }
+
+        private void rotateOX_Click(object sender, EventArgs e)
+        {
+            rotateOY.Checked = rotateOZ.Checked = rotateOwn.Checked = false;
+        }
+
+        private void rotateOY_Click(object sender, EventArgs e)
+        {
+            rotateOX.Checked = rotateOZ.Checked = rotateOwn.Checked = false;
+        }
+
+        private void rotateOZ_Click(object sender, EventArgs e)
+        {
+            rotateOY.Checked = rotateOX.Checked = rotateOwn.Checked = false;
+        }
+        private void rotateOwn_Click(object sender, EventArgs e)
+        {
+            rotateOY.Checked = rotateOZ.Checked = rotateOX.Checked = false;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fName = saveFileDialog1.FileName;
+                File.WriteAllText(fName, JsonConvert.SerializeObject(curFigure, Formatting.Indented), Encoding.UTF8);
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fName = openFileDialog1.FileName;
+                if (File.Exists(fName))
+                {
+                    curFigure = JsonConvert.DeserializeObject<Figure>(File.ReadAllText(fName, Encoding.UTF8));
+                    Draw();
+                }
+            }
+        }
+
+
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+            int count = int.Parse(textBox5.Text);
+            char axis;
+            if (radioButton1.Checked)
+            {
+                axis = 'x';
+            }
+            else if (radioButton2.Checked)
+            {
+                axis = 'y';
+            }
+            else
+            {
+                axis = 'z';
+            }
+            curFigure = FigureRotation.CreateRotationFigure(rotationPoints, count, axis);
+            Draw();
+        }
+
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            radioButton2.Checked = radioButton3.Checked = false;
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = radioButton3.Checked = false;
+        }
+
+        private void radioButton3_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = radioButton2.Checked = false;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            float x = float.Parse(textBox6.Text);
+            float y = float.Parse(textBox7.Text);
+            float z = float.Parse(textBox8.Text);
+
+            rotationPoints.Add(new Point3D(x, y, z));
+        }
+        delegate float func(float x, float y);
+        private void button16_Click(object sender, EventArgs e)
+        {
+            float x0 = 0, y0 = 0, x1 = 0, y1 = 0, count = 0;
+            try
+            {
+                float.TryParse(textBox9.Text, out x0);
+                float.TryParse(textBox10.Text, out y0);
+                float.TryParse(textBox11.Text, out x1);
+                float.TryParse(textBox12.Text, out y1);
+                float.TryParse(textBox13.Text, out count);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Неверное значение для графика");
+                return;
+            }
+            func f;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    f = (x, y) => (float)(Cos(x + y));
+                    break;
+                case 1:
+                    f = (x, y) => (float)(Sin(x + y));
+                    break;
+                default:
+                    MessageBox.Show("График не выбран");
+                    return;
+            }
+            float dx = (x1 - x0) / count;
+            float dy = (y1 - y0) / count;
+            float curx, cury = y0;
+
+            List<Point3D> points = new List<Point3D>();
+            for (int i = 0; i <= count; i++)
+            {
+                curx = x0;
+                for (int j = 0; j <= count; j++)
+                {
+                    points.Add(new Point3D(curx, cury, f(curx, cury)));
+                    curx += dx;
+                }
+                cury += dy;
+            }
+            Figure figure = new Figure(points);
+            int n = (int)count + 1;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (j != n - 1)
+                        figure.AddEdges(i * n + j, i * n + j + 1);
+                    if (i != n - 1)
+                        figure.AddEdges(i * n + j, (i + 1) * n + j);
+                    if (i != n - 1 && j != n - 1)
+                        figure.AddSurface(new List<int> { i * n + j, i * n + j + 1, (i + 1) * n + j, (i + 1) * n + j + 1 });
+                }
+
+            AffineChanges.ScaleCenter(figure, 40);
+            AffineChanges.RotateCentral(figure, 60, 0, 0);
+            curFigure = figure;
+            Draw();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void ColorizedZBuffer(List<Color> colors)
+        {
+            Bitmap bmp = Buffer.CreateZBuffer(pictureBox1.Width, pictureBox1.Height, scenes, colors, projBox.SelectedIndex);
+            pictureBox1.Image = bmp;
+            pictureBox1.Invalidate();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (checkBox1.Checked)
+            {
+                Draw();
+                ColorizedZBuffer(colors);
+            }
+            else
+            {
+                Draw();
+
+            }
         }
     }
 }
